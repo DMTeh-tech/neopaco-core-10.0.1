c089a19179963125d2be24a6dfb3f4a8506c1597
DMTeh-tech <DMT_eh@icloud.com>
2026-01-27 06:12:47 +0400

    Update containment.py

--- src/core/topology/containment.py
+++ src/core/topology/containment.py
@@ -1,10 +1,40 @@
 import numpy as np
 
 class S3Containment:
+    """
+    Модель семантического удержания на базе геометрии S3/T4.
+    Реализация от 27 января 2026 года.
+    """
     def __init__(self, matrix_27):
-        self.matrix = matrix_27 # Матрица 27x27
-        self.radius = self.calculate_radius()
+        self.matrix = matrix_27  # Матрица 27x27 (Спектр системы)
+        self.R = self.calculate_radius() # Радиус семантического удержания
 
     def calculate_radius(self):
-        # Математика: R = sqrt(Tr(M* M))
+        """
+        Аксиома IV: Вычисление R через спектральную норму.
+        R определяет максимальный объем информации за один Хронон.
+        """
         return np.sqrt(np.trace(self.matrix.T @ self.matrix))
+
+    def generate_chronon(self, theta):
+        """
+        Генерация Хронона через вращение Тесеракта.
+        Один такт (t) = угол поворота dTheta внутри гиперсферы S3.
+        """
+        c, s = np.cos(theta), np.sin(theta)
+        # Матрица вращения в 4D (две плоскости вращения для Тесеракта)
+        rotation_4d = np.array([
+            [c, -s, 0,  0],
+            [s,  c, 0,  0],
+            [0,  0, c, -s],
+            [0,  0, s,  c]
+        ])
+        return rotation_4d
+
+    def check_homeostasis(self, force_t4):
+        """
+        Аксиома IV: Закон сопротивления кривизны.
+        P_res ∝ 1/R^2 против динамики Тесеракта T4.
+        """
+        p_res = 1 / (self.R ** 2)
+        return p_res > force_t4
